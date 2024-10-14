55af6722554a2567e9e0c812b8a6eab07fc70f5c
Tom Fanning <tom@m0lte.uk>
2024-10-14 21:57:02 +0100

    Sort out Steve crash

--- bpqapi-tests/WebmailResponseParserTests.cs
+++ bpqapi-tests/WebmailResponseParserTests.cs
@@ -8,7 +8,7 @@ public class WebmailResponseParserTests
     [Fact]
     public void TestPageParsing()
     {
-        var html = File.ReadAllText("testdata/webmaillistingpage.html");
+        var html = File.ReadAllText("testdata/webmaillistingpage-01.html");
         var result = WebmailListingParser.Parse(html);
         result.Success.Should().BeTrue();
         result.Input.Should().BeNullOrWhiteSpace();
@@ -32,5 +32,66 @@ public class WebmailResponseParserTests
         mail.Last().To.Should().Be("NTSxxxx");
         mail.Last().At.Should().Be("ARRLxxx");
         mail.Last().From.Should().Be("CX2SAxx");
+    }
+
+    [Fact]
+    public void TestPageParsing02()
+    {
+        var html = File.ReadAllText("testdata/webmaillistingpage-02.html");
+        var result = WebmailListingParser.Parse(html);
+        result.Success.Should().BeTrue();
+        result.Input.Should().BeNullOrWhiteSpace();
+
+        var (token, mail) = result.Value;
+
+        mail[0].Id.Should().Be(6876);
+        mail[0].From.Should().Be("CX2SA");
+        mail[0].To.Should().Be("NTS");
+        mail[0].At.Should().Be("ARRL");
+        mail[0].Date.Month.Should().Be(10);
+        mail[0].Date.Day.Should().Be(16);
+        mail[0].Type.Should().Be('B');
+        mail[0].State.Should().Be('N');
+        mail[0].Length.Should().Be(26386);
+        mail[0].Subject.Should().Be("October 2024 NTS Letter");
+
+        mail.Should().HaveCount(221);
+
+        mail.Last().Id.Should().Be(1111);
+        mail.Last().To.Should().Be("NTSxxxx");
+        mail.Last().At.Should().Be("ARRLxxx");
+        mail.Last().From.Should().Be("CX2SAxx");
+    }
+
+    [Fact]
+    public void ParseWebmailLineTest01()
+    {
+        var line = " 13-Jun BF 10946 DX      WW      LU9DCE  DX OPERATION 13-JUN24";
+        var mail = WebmailListingParser.ParseMailItem(line, 1);
+        mail.Date.Day.Should().Be(13);
+        mail.Date.Month.Should().Be(6);
+        mail.Type.Should().Be('B');
+        mail.State.Should().Be('F');
+        mail.Length.Should().Be(10946);
+        mail.To.Should().Be("DX");
+        mail.At.Should().Be("WW");
+        mail.From.Should().Be("LU9DCE");
+        mail.Subject.Should().Be("DX OPERATION 13-JUN24");
+    }
+
+    [Fact]
+    public void ParseWebmailLineTests02()
+    {
+        var line = " 03-Jun BF  3731 KEP     WW      NC8Q    ";
+        var mail = WebmailListingParser.ParseMailItem(line, 1);
+        mail.Date.Day.Should().Be(3);
+        mail.Date.Month.Should().Be(6);
+        mail.Type.Should().Be('B');
+        mail.State.Should().Be('F');
+        mail.Length.Should().Be(3731);
+        mail.To.Should().Be("KEP");
+        mail.At.Should().Be("WW");
+        mail.From.Should().Be("NC8Q");
+        mail.Subject.Should().BeEmpty();
     }
 }
--- bpqapi/Parsers/WebmailListingParser.cs
+++ bpqapi/Parsers/WebmailListingParser.cs
@@ -21,33 +21,7 @@ public class WebmailListingParser
             var year = DateTime.UtcNow.Year;
             foreach (var a in anchors)
             {
-                var text = a.NextSibling.InnerText.Trim();
-
-                // 06-Oct BN 26386 NTS     ARRL    CX2SA   October 2024 NTS Letter
-
-                var id = a.InnerText;
-                var day = text[..2];
-                var month = text[3..6];
-                var state = text[7..9].Trim();
-                var len = text[10..15];
-                var to = text[16..24];
-                var at = text[24..32];
-                var from = text[32..40];
-                var subject = text[40..];
-
-                var mailItem = new MailListEntity
-                {
-                    Id = int.Parse(id),
-                    Date = new MonthAndDay(months[month], int.Parse(day)),
-                    Type = state[0],
-                    State = state[1],
-                    Length = int.Parse(len),
-                    To = to.Trim(),
-                    At = at.Trim(),
-                    From = from.Trim(),
-                    Subject = subject.Trim()
-                };
-
+                var mailItem = ParseMailItem(a.NextSibling.InnerText, int.Parse(a.InnerText));
                 mailItems.Add(mailItem);
             }
 
@@ -57,6 +31,34 @@ public class WebmailListingParser
         {
             return ParseResult<(string token, MailListEntity[] mail)>.CreateFailure(ex, html);
         }
+    }
+
+    public static MailListEntity ParseMailItem(string text, int id)
+    {
+        // 06-Oct BN 26386 NTS     ARRL    CX2SA   October 2024 NTS Letter
+        // 03-Jun BF  3731 KEP     WW      NC8Q    
+
+        var day = text[1..3];
+        var month = text[4..7];
+        var state = text[8..10].Trim();
+        var len = text[11..16];
+        var to = text[17..25];
+        var at = text[25..33];
+        var from = text[33..41];
+        var subject = text[41..];
+
+        return new MailListEntity
+        {
+            Id = id,
+            Date = new MonthAndDay(months[month], int.Parse(day)),
+            Type = state[0],
+            State = state[1],
+            Length = int.Parse(len),
+            To = to.Trim(),
+            At = at.Trim(),
+            From = from.Trim(),
+            Subject = subject.Trim()
+        };
     }
 
     internal static ParseResult<bool> ParseMaybeLoginPage(string html)
