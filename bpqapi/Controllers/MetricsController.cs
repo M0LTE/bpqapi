ce8acdb394b3d9d7644f085df81526c76046a025
Tom M0LTE <tom@m0lte.uk>
2024-10-14 19:20:18 +0100

    Add endpoint suitable for Home Assistant REST sensor

--- bpqapi/Controllers/MetricsController.cs
+++ bpqapi/Controllers/MetricsController.cs
@@ -1,4 +1,5 @@
 ﻿using Aspects.Cache;
+using bpqapi.Models;
 using bpqapi.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Reflection.PortableExecutable;
@@ -25,9 +26,48 @@ public class MetricsController(BpqUiService bpqUiService) : ControllerBase
 
         try
         {
-            var data = await GetData(header.Value.User, header.Value.Password);
+            var partners = await GetCachedMailPartners(header.Value.User, header.Value.Password);
 
-            return Ok(data);
+            /*
+             * timestamp is nanoseconds
+             * 
+    citibike,station_id=4703 eightd_has_available_keys=false,is_installed=1,is_renting=1,is_returning=1,legacy_id="4703",num_bikes_available=6,num_bikes_disabled=2,num_docks_available=26,num_docks_disabled=0,num_ebikes_available=0,station_status="active" 1641505084000000000
+    citibike,station_id=4704 eightd_has_available_keys=false,is_installed=1,is_renting=1,is_returning=1,legacy_id="4704",num_bikes_available=10,num_bikes_disabled=2,num_docks_available=36,num_docks_disabled=0,num_ebikes_available=0,station_status="active" 1641505084000000000
+    citibike,station_id=4711 eightd_has_available_keys=false,is_installed=1,is_renting=1,is_returning=1,legacy_id="4711",num_bikes_available=9,num_bikes_disabled=0,num_docks_available=36,num_docks_disabled=0,num_ebikes_available=1,station_status="active" 1641505084000000000
+             */
+
+            var sb = new StringBuilder();
+
+            var ns = (DateTime.UtcNow - DateTime.UnixEpoch).TotalNanoseconds;
+
+            foreach (var partner in partners)
+            {
+                sb.Append($"packetmail,partner={partner.Callsign} queue_length={partner.QueueLength} {ns:0}\n");
+            }
+
+            return Ok(sb.ToString());
+        }
+        catch (LoginFailedException)
+        {
+            return Unauthorized(LoginError);
+        }
+    }
+
+    [HttpGet("json/mail")]
+    public async Task<IActionResult> GetJsonMetrics()
+    {
+        var header = HttpContext.ParseBasicAuthHeader();
+
+        if (header == null)
+        {
+            return BadRequest(AuthError);
+        }
+
+        try
+        {
+            var partners = await GetCachedMailPartners(header.Value.User, header.Value.Password);
+
+            return Ok(partners.ToDictionary(item => item.Callsign, item => new { queueLength = item.QueueLength }));
         }
         catch (LoginFailedException)
         {
@@ -36,27 +76,10 @@ public class MetricsController(BpqUiService bpqUiService) : ControllerBase
     }
 
     [MemoryCache(59)]
-    private async Task<string> GetData(string user, string password)
+    private async Task<ForwardingStation[]> GetCachedMailPartners(string user, string password)
     {
         var partners = await bpqUiService.GetMailForwardingPartners(user, password);
 
-        /*
-         * timestamp is nanoseconds
-         * 
-citibike,station_id=4703 eightd_has_available_keys=false,is_installed=1,is_renting=1,is_returning=1,legacy_id="4703",num_bikes_available=6,num_bikes_disabled=2,num_docks_available=26,num_docks_disabled=0,num_ebikes_available=0,station_status="active" 1641505084000000000
-citibike,station_id=4704 eightd_has_available_keys=false,is_installed=1,is_renting=1,is_returning=1,legacy_id="4704",num_bikes_available=10,num_bikes_disabled=2,num_docks_available=36,num_docks_disabled=0,num_ebikes_available=0,station_status="active" 1641505084000000000
-citibike,station_id=4711 eightd_has_available_keys=false,is_installed=1,is_renting=1,is_returning=1,legacy_id="4711",num_bikes_available=9,num_bikes_disabled=0,num_docks_available=36,num_docks_disabled=0,num_ebikes_available=1,station_status="active" 1641505084000000000
-         */
-
-        var sb = new StringBuilder();
-
-        var ns = (DateTime.UtcNow - DateTime.UnixEpoch).TotalNanoseconds;
-
-        foreach (var partner in partners)
-        {
-            sb.Append($"packetmail,partner={partner.Callsign} queue_length={partner.QueueLength} {ns:0}\n");
-        }
-
-        return sb.ToString();
+        return partners;
     }
 }
