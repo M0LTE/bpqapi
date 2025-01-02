693b12eb66ae4cb00e0d5afad1905f4e99bc920b
Tom M0LTE <tom@m0lte.uk>
2025-01-02 22:18:55 +0000

    Add nlog

--- bpqapi/Program.cs
+++ bpqapi/Program.cs
@@ -1,75 +1,99 @@
 using bpqapi;
 using bpqapi.Services;
 using Microsoft.OpenApi.Models;
+using NLog;
+using NLog.Web;
 using System.Net;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-// Add services to the container.
-
-builder.Services.AddHostedService<BpqConnectivityCheck>();
-builder.Services.AddHostedService<ConfigCheckService>();
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+try
 {
-    var filePath = Path.Combine(AppContext.BaseDirectory, "bpqapi.xml");
-    c.IncludeXmlComments(filePath);
-});
-builder.Services.Configure<BpqApiOptions>(builder.Configuration.GetSection("bpq"));
-builder.Services.AddSingleton<BpqUiService>();
-builder.Services.AddSingleton<BpqNativeApiService>();
-builder.Services.AddSingleton<BpqMailCache>();
-builder.Services.AddTransient<BpqTelnetClient>();
-builder.Services.AddHttpClient<BpqUiService>(client =>
-{
-    client.Timeout = TimeSpan.FromSeconds(5);
-}).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip });
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpClient<BpqNativeApiService>(client =>
-{
-    client.Timeout = TimeSpan.FromSeconds(5);
-}).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip });
+    // Add services to the container.
 
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BPQ API", Version = "v1" });
-    c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+    builder.Services.AddHostedService<BpqConnectivityCheck>();
+    builder.Services.AddHostedService<ConfigCheckService>();
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
     {
-        Name = "Basic authorisation",
-        Type = SecuritySchemeType.Http,
-        Scheme = "basic",
-        In = ParameterLocation.Header,
-        Description = "Basic Authorization header using the Bearer scheme."
+        var filePath = Path.Combine(AppContext.BaseDirectory, "bpqapi.xml");
+        c.IncludeXmlComments(filePath);
     });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement { { new OpenApiSecurityScheme { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "basic" } }, [] } });
-});
+    builder.Services.Configure<BpqApiOptions>(builder.Configuration.GetSection("bpq"));
+    builder.Services.AddSingleton<BpqUiService>();
+    builder.Services.AddSingleton<BpqNativeApiService>();
+    builder.Services.AddSingleton<BpqMailCache>();
+    builder.Services.AddTransient<BpqTelnetClient>();
+    builder.Services.AddHttpClient<BpqUiService>(client =>
+    {
+        client.Timeout = TimeSpan.FromSeconds(5);
+    }).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip });
 
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
-});
+    builder.Services.AddHttpClient<BpqNativeApiService>(client =>
+    {
+        client.Timeout = TimeSpan.FromSeconds(5);
+    }).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip });
 
-builder.Services.Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(options =>
-{
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
-});
 
-var app = builder.Build();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "BPQ API", Version = "v1" });
+        c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+        {
+            Name = "Basic authorisation",
+            Type = SecuritySchemeType.Http,
+            Scheme = "basic",
+            In = ParameterLocation.Header,
+            Description = "Basic Authorization header using the Bearer scheme."
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement { { new OpenApiSecurityScheme { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "basic" } }, [] } });
+    });
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+    builder.Services.ConfigureHttpJsonOptions(options =>
+    {
+        options.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+    });
+
+    builder.Services.Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+    });
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    //if (app.Environment.IsDevelopment())
+    //{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+    //}
 
-app.UseAuthorization();
+    app.UseAuthorization();
 
-app.MapControllers();
+    app.MapControllers();
 
-app.Run();
+    app.Run();
+
+}
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    LogManager.Shutdown();
+}
